7821f8f26ee724721f234f6ac9f1668093ada9e4
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-03-08 02:52:16 +0300

    Add CreateArray function

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray()
+{
+	return new string[]{ "hello", "2", "world", ":-)" };
+}
